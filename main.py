cdde1579c1af7d39d387fa85a160793fcaa2af6e
Olaf Stachowski <138628377+olstach@users.noreply.github.com>
2023-08-10 22:26:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+import typer
+import sys, random, time
+import DivItems
+
+app = typer.Typer()
+def roll():
+    bija = {
+        '1': 'DHIH',
+        '2': 'RA',
+        '3': 'PA',
+        '4': 'TSA',
+        '5': 'NA',
+        '6': 'AH'
+    }
+
+    lot = (random.randint(1, 6), random.randint(1, 6))
+    result = [bija[str(item)] for item in lot]
+
+    for item in result:
+        print(item, end=' ', flush=True)
+        time.sleep(1)
+
+    result_string = "_".join(result)
+    item_function = getattr(DivItems, result_string)
+
+    time.sleep(1)
+    item_function.submenu()
+
+def check():
+    check = input("Enter the syllables of the divination: ")
+    if '_' not in check:
+        check = check.replace(" ", "_")
+    item_check = getattr(DivItems, check)
+    item_check.submenu()
+
+
+
+def menu():
+    print("\n MO DIVINATION"+ '\n')
+    time.sleep(2)
+    option = int(input("\n [1] Ask a question \n [2] Check results \n [3] Quit \n "))
+
+    if option == 1:
+        input("Focus on the question and press Enter... ")
+        roll()
+        time.sleep(2)
+    elif option == 2:
+        check()
+    elif option == 3:
+        print("\n Good bye! \n")
+        sys.exit()
+
+
+while True:
+    menu()
+
+if __name__ == "__main__":
+    app()
+
+#TODO: try-except blocks for invalid keyboard input in menu & submenu
+#TODO: explore Typer options for running from CLI
+#TODO: how to package this into a cli app that I could share?
